5a402696754181e93a929c843131e5798ec53be1
ewel1991 <ewelina.beben.programista@gmail.com>
2025-08-03 16:20:29 +0200

    index.js modified

--- backend/index.js
+++ backend/index.js
@@ -347,7 +347,7 @@ app.delete("/notes/:id", ensureAuthenticated, async (req, res) => {
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
 });
 
 app.listen(port, "0.0.0.0", () => {
